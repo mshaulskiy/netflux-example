31e8b4f499ff488afe13330fab3beb625b6eebf9
Mikhail Shaulskiy <mshaulskiy@gmail.com>
2017-11-20 04:39:33 +0000

    Created REST Endpoint

--- src/main/java/com/shaulskiy/netfluxexample/controller/MovieController.java
+++ src/main/java/com/shaulskiy/netfluxexample/controller/MovieController.java
@@ -1,4 +1,37 @@
 package com.shaulskiy.netfluxexample.controller;
 
+import com.shaulskiy.netfluxexample.domain.Movie;
+import com.shaulskiy.netfluxexample.domain.MovieEvent;
+import com.shaulskiy.netfluxexample.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping(value = "/{id}/events")
+    Flux<MovieEvent> streamMovieEvents(@PathVariable String id){
+        return movieService.events(id);
+    }
+
+    @GetMapping(value = "/{id}")
+    Mono<Movie> getMovieById(@PathVariable String id){
+        return movieService.getMovieById(id);
+    }
+
+    @GetMapping
+    Flux<Movie> getAllMovies(){
+        return movieService.getAllMovies();
+    }
 }
